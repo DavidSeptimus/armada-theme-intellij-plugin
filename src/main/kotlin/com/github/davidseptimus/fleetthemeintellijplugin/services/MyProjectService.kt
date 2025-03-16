865238df3be6668ce687c6cdf8446850a9d08692
GitHub Action <action@github.com>
2025-03-16 22:57:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/davidseptimus/fleetthemeintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/davidseptimus/fleetthemeintellijplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.davidseptimus.fleetthemeintellijplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.davidseptimus.fleetthemeintellijplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
