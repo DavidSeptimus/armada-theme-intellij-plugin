f513614135fb23603fd3a587479d244d0a060a81
David Septimus <davidseptimus@gmail.com>
2025-03-16 20:06:24 -0600

    Add Fleet Dark theme and XML annotator support

---
+++ src/main/kotlin/com/github/davidseptimus/fleetthemeintellijplugin/FleetXmlAnnotator.kt
@@ -0,0 +1,49 @@
+
+package com.github.davidseptimus.fleetthemeintellijplugin
+
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.psi.xml.XmlDoctype
+import com.intellij.psi.xml.XmlToken
+import com.intellij.psi.xml.XmlTokenType
+
+
+class FleetXmlAnnotator : Annotator {
+    companion object {
+        val doctypeAttributes = TextAttributesKey.createTextAttributesKey("XML_DOCTYPE")
+    }
+
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        annotateDocType(element, holder)
+    }
+
+    private fun annotateDocType(element: PsiElement, holder: AnnotationHolder) {
+            if (element is XmlToken && element.tokenType == XmlTokenType.XML_DOCTYPE_START) {
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                    .range(TextRange(element.textRange.startOffset, element.textRange.endOffset))
+                    .textAttributes(doctypeAttributes)
+                    .create()
+            return
+            }
+
+            if (element is XmlToken && element.tokenType == XmlTokenType.XML_DOCTYPE_END) {
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                    .range(TextRange(element.textRange.startOffset, element.textRange.endOffset))
+                    .textAttributes(doctypeAttributes)
+                    .create()
+                return
+            }
+
+            if (element is XmlToken && element.tokenType == XmlTokenType.XML_DOCTYPE_SYSTEM) {
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                    .range(TextRange(element.textRange.startOffset, element.textRange.endOffset))
+                    .textAttributes(doctypeAttributes)
+                    .create()
+                return
+            }
+    }
+}
