dfaf345c43c249421b899fe47bc9b5714db3ffbd
David Septimus <davidseptimus@gmail.com>
2025-07-19 14:35:09 -0600

    Updates build process dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,7 @@ fun IntelliJPlatformDependenciesExtension.pluginsInLatestCompatibleVersion(plugi
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
     testImplementation(libs.junit)
+    testImplementation(libs.opentest4j)
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
@@ -70,8 +71,6 @@ dependencies {
         pluginsInLatestCompatibleVersion(
             providers.gradleProperty("platformPluginsLatestCompatibleVersion").map { it.split(',') })
 
-        pluginVerifier("1.388")
-        zipSigner()
         testFramework(TestFrameworkType.Platform)
     }
 }
