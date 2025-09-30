99b73cceb07aed52d50b719e7da2c1ac7bc3e213
David Septimus <davidseptimus@gmail.com>
2025-09-30 12:14:57 -0600

    feat: improves SQL  reference identifier detection

--- src/main/kotlin/com/github/davidseptimus/armada/annotators/SQLAnnotator.kt
+++ src/main/kotlin/com/github/davidseptimus/armada/annotators/SQLAnnotator.kt
@@ -11,7 +11,7 @@ import com.intellij.sql.psi.impl.SqlPrimaryKeyDefinitionImpl
 
 class SQLAnnotator : Annotator {
 
-    val sqlSymbolicOperators = setOf(
+    private val sqlSymbolicOperators = setOf(
         "=",
         "<",
         ">",
@@ -46,55 +46,83 @@ class SQLAnnotator : Annotator {
         "%="
     )
 
+    private val siblingPredicate = { s: PsiElement -> s.text == "." }
+
     override fun isDumbAware(): Boolean {
         return true
     }
 
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        val siblingPredicate = { s: PsiElement -> s.text == "." }
+
+        var annotated = false
         when {
             sqlSymbolicOperators.contains(element.text) -> {
                 highlightElement(element, holder, TextAttributeKeys.SQL_SYMBOLIC_OPERATORS)
+                annotated = true
             }
 
             element is SqlPrimaryKeyDefinitionImpl -> {
                 highlightElement(element, holder, TextAttributeKeys.SQL_PRIMARY_KEY_DEFINITION)
+                annotated = true
             }
-            (element is SqlIdentifier || element is SqlNameElement )&& hasAncestor(element, {a: PsiElement -> (a.elementType == SqlElementTypes.SQL_TABLE_REFERENCE && a.parent is SqlCreateTableStatement) || (a.elementType == SqlElementTypes.SQL_SCHEMA_REFERENCE && a.parent is SqlCreateSchemaStatement)}) -> {
+            (element is SqlIdentifier || element is SqlNameElement )&& !hasAncestor(element, {p: PsiElement -> p.elementType == SqlElementTypes.SQL_TABLE_ELEMENT_LIST }) && hasAncestor(element, { a: PsiElement -> (a.elementType == SqlElementTypes.SQL_TABLE_REFERENCE && a.parent is SqlCreateTableStatement) || (a.elementType == SqlElementTypes.SQL_SCHEMA_REFERENCE && a.parent is SqlCreateSchemaStatement)}) -> {
                 highlightElement(element, holder, TextAttributeKeys.SQL_SCHEMA_TABLE_IDENTIFIER)
+                annotated = true
             }
             hasAncestor(element, SqlForeignKeyDefinition::class.java) -> {
-                annotateForeignKey(element, holder)
-            }
-            (element is SqlIdentifier || element is SqlNameElement) && hasAncestor(
-                element,
-                SqlReferenceExpression::class.java
-            )
-                    && (element.nextSibling?.text == "." || hasSibling(element.parent, siblingPredicate)  || element.prevSibling?.text == "." || hasSibling(
-                element,
-                siblingPredicate,
-                true
-            )) -> {
-                highlightElement(element, holder, TextAttributeKeys.SQL_REFERENCE_IDENTIFIER)
+                annotated = annotateForeignKey(element, holder)
             }
         }
+        if (annotated) {
+            return
+        }
+        annotateReferenceIdentifier(element, holder)
+
     }
 
-    val fkReferenceClauseText = "SQL_FOREIGN_KEY_REFERENCES_CLAUSE"
-    val fkCascadeClauseText = "SQL_FOREIGN_KEY_CASCADE_OPTION"
-    val fkCascadeKeywords = setOf("cascade", "update", "on", "delete")
-
+    private val fkReferenceClauseString = "SQL_FOREIGN_KEY_REFERENCES_CLAUSE"
+    private val fkCascadeClauseString = "SQL_FOREIGN_KEY_CASCADE_OPTION"
+    private val fkCascadeKeywords = setOf("cascade", "update", "on", "delete")
 
     private fun annotateForeignKey(
         element: PsiElement,
         holder: AnnotationHolder
-    ) {
+    ): Boolean {
         val text = element.text.lowercase()
+        if (annotateReferenceIdentifier(element, holder)) {
+            return true
+        }
         if (text == "foreign" || text == "key"
-            || (text == "references" && element.parent.toString() == fkReferenceClauseText)
-            || (fkCascadeKeywords.contains(text) && element.parent.toString() == fkCascadeClauseText)
+            || (text == "references" && element.parent.toString() == fkReferenceClauseString)
+            || (fkCascadeKeywords.contains(text) && element.parent.toString() == fkCascadeClauseString)
         ) {
             highlightElement(element, holder, TextAttributeKeys.SQL_FOREIGN_KEY_DEFINITION)
+            return true
         }
+        return false
+    }
+
+    private fun annotateReferenceIdentifier(
+        element: PsiElement,
+        holder: AnnotationHolder
+    ): Boolean {
+        if ((element is SqlIdentifier || element is SqlNameElement)
+            && hasAncestor(
+                element,
+                SqlReferenceExpression::class.java
+            )
+            && (element.nextSibling?.text == "." || hasSibling(
+                element.parent,
+                siblingPredicate
+            ) || element.prevSibling?.text == "." || hasSibling(
+                element,
+                siblingPredicate,
+                true
+            ))
+        ) {
+            highlightElement(element, holder, TextAttributeKeys.SQL_REFERENCE_IDENTIFIER)
+            return true
+        }
+        return false
     }
 }
